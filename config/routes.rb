7a8956e86f59021a7ddde71b1a4fd080837d30db
Pedro Pazello <email@pedropazello.com>
2021-02-22 19:17:35 -0300

    add sidekiq admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => "/sidekiq"
+
   root to: "home#index"
   post "/increment_async", to: "home#increment_async"
 end
